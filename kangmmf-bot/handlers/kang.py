baf4b9bcaa7f4fe5a636244e9805071af818d898
phoenix-cmd <73835333+phoenix-cmd@users.noreply.github.com>
2025-05-27 18:20:35 +0530

    Update kang.py

--- kangmmf-bot/handlers/kang.py
+++ kangmmf-bot/handlers/kang.py
@@ -16,8 +16,7 @@ import logging
 from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.handlers import MessageHandler
-from utils.sticker_utils import kang_sticker
-from utils.logger import log_to_channel  
+from utils.sticker_utils import kang_sticker  
 
 async def kang_command(client, message: Message):
     if not message.reply_to_message:
@@ -25,10 +24,6 @@ async def kang_command(client, message: Message):
         return
 
     target = message.reply_to_message
-    await kang_sticker(client, message, target)
-
-    await log_to_channel(client, message, command="kang")
-    logging.info(f"/kang used by user {message.from_user.id} in chat {message.chat.id}")
 
 kang_handler = MessageHandler(kang_command, filters.command("kang"))
 
