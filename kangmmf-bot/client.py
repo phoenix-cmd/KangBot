23d5547a963efd61850a6b16324a678567f51a87
phoenix-cmd <73835333+phoenix-cmd@users.noreply.github.com>
2025-05-27 17:34:06 +0530

    Update client.py

--- kangmmf-bot/client.py
+++ kangmmf-bot/client.py
@@ -1,16 +1,5 @@
-# # client.py
-# import os
-# from pyrogram import Client
-
-# app = Client(
-#     "my_bot",
-#     api_id=int(os.environ["API_ID"]),
-#     api_hash=os.environ["API_HASH"],
-#     bot_token=os.environ["BOT_TOKEN"]
-# )
 # client.py
 import os
-import logging
 from pyrogram import Client
 
 app = Client(
@@ -19,13 +8,3 @@ app = Client(
     api_hash=os.environ["API_HASH"],
     bot_token=os.environ["BOT_TOKEN"]
 )
-
-# Setup logging to file
-logging.basicConfig(
-    filename="bot.log",      # Log file name
-    filemode="a",            # Append mode
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    level=logging.INFO       # Log level
-)
-
-logging.info("Bot started")  # Log bot start event
