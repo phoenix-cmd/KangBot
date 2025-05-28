38fc460da9b7a05e6e7e3995856b25ccee5aa853
phoenix-cmd <73835333+phoenix-cmd@users.noreply.github.com>
2025-05-28 13:13:26 +0530

    Update chatbot.py

--- kangmmf-bot/chatbot.py
+++ kangmmf-bot/chatbot.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 API_KEY = os.getenv("GOOGLE_GEMINI_API_KEY")
 GOOGLE_GEMINI_API_KEY = os.getenv("GOOGLE_GEMINI_API_KEY")
-GOOGLE_GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}"
+GOOGLE_GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
 
 CHATBOT_TOGGLE_FILE = "enabled_chats.json"
 
